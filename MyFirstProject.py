55ea4b156848716d05a51638191a7beae7743878
Swamh <58577548+Swamh@users.noreply.github.com>
2023-10-09 10:39:10 +0800

    My First Project
    Hyperskill Project

---
+++ MyFirstProject.py
@@ -0,0 +1,21 @@
+# Write your code here
+print("Earned amount:")
+Bubblegum=202
+print("Bubblegum: $",Bubblegum)
+Toffee=118
+print("Toffee:: $",Toffee)
+Icecream=2250
+print("Ice cream: $",Icecream)
+Milkchocolate=1680
+print("Milk chocolate: $",Milkchocolate)
+Doughnut=1075
+print("Doughnut: $",Doughnut)
+Pancake=80
+print("Pancake: $",Pancake)
+print("")
+Income=Bubblegum+Toffee+Icecream+Milkchocolate+Doughnut+Pancake
+print("Income: $",Income)
+StaffE=int(input("Staff expenses:"))
+OtherE=int(input("Other expenses:"))
+NetIncome=Income-StaffE-OtherE
+print("Net income: $",NetIncome)
